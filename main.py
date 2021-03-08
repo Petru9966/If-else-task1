0f1fff946e4429947762286700dcae10661b95b5
repl.it user <replituser@example.com>
2021-03-08 05:54:15 +0000

    Task if else lesson6

---
+++ main.py
@@ -0,0 +1,8 @@
+#Determine the type of activity
+print("Please enter the activity to be performed!")
+activity=input()
+if(activity== "calculate" ):
+  print("Performing calculations")
+else:
+  print("Performing other type of activity!")
+print("Activity comleted")
